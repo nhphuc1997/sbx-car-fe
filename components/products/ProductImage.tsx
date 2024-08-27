98da291216f40a84038373757a50bc257d1cc75f
phucnh17 <phucnh17@msb.com.vn>
2024-08-27 16:29:21 +0700

    feat: i18n

--- components/products/ProductImage.tsx
+++ components/products/ProductImage.tsx
@@ -1,18 +1,46 @@
-import { Affix, Col, Image, Row } from "antd";
+import { Affix, Col, Image, Row, Skeleton } from "antd";
 import Order from "../orders/Order";
 import BookATestDriver from "../book-a-test-driver/BookATestDriver";
+import { useParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import { doGet } from "@/utils/doMethod";
+import { map } from "lodash";
+import { S3_URL } from "@/utils/aws";
 
-export default function ProductImage() {
-  const assets = [
-    "/assets/car1.jpeg",
-    "/assets/car2.jpeg",
-    "/assets/car3.jpeg",
-    "/assets/car4.jpeg",
-    "/assets/car5.jpeg",
-    "/assets/car6.jpeg",
-    "/assets/car7.jpeg",
-    "/assets/car8.jpeg",
-  ];
+interface Props {
+  dataInfor?: Record<string, any>;
+}
+
+export default function ProductImage({ dataInfor }: Props) {
+  const { id } = useParams();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-exterior", [id]],
+    queryFn: async () =>
+      await doGet("/exteriors", {
+        s: JSON.stringify({
+          carId: id,
+        }),
+      }),
+  });
+
+  if (isLoading) {
+    return (
+      <div>
+        <Row gutter={8}>
+          <Col xs={24} md={18} className="pt-2">
+            <Skeleton.Image active={true} className="!w-full !h-[500px]" />
+          </Col>
+          <Col xs={24} md={6} className="pt-2 space-y-5">
+            <Skeleton.Input active={true} className="!h-[110px] !w-full" />
+            <Skeleton.Input active={true} className="!h-[110px] !w-full" />
+            <Skeleton.Input active={true} className="!h-[110px] !w-full" />
+            <Skeleton.Input active={true} className="!h-[110px] !w-full" />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 
   return (
     <div className="">
@@ -20,26 +48,28 @@ export default function ProductImage() {
         <Col xs={24} md={18} className="h-[500px] pt-2">
           <img
             className="!w-full h-full object-cover"
-            src="https://i.sbxcars.com/cdn-cgi/image/width=900,height=1352,quality=80/auctions/60Lc8taIaaBSV9sGL512F3XpWzqXTF3Z/SBXHyperionVertical001.JPG"
+            src={`${S3_URL}/${dataInfor?.s3Key}`}
             alt=""
           />
         </Col>
 
         <Col xs={24} md={6} className="h-[500px] pt-2">
           <Affix offsetTop={12}>
-            <Image.PreviewGroup>
-              <Row gutter={8} className="h-[400px] overflow-y-auto">
-                {assets.map((asset, index: number) => (
-                  <Col span={12} key={index}>
-                    <Image src={asset} alt="" />
-                  </Col>
-                ))}
-              </Row>
-            </Image.PreviewGroup>
-            <div className="h-[92px] flex flex-col items-end justify-end">
-              <Order />
-              <div className="py-1" />
-              <BookATestDriver />
+            <div>
+              <Image.PreviewGroup>
+                <Row gutter={8} className="h-[400px] overflow-y-auto">
+                  {map(data?.data, (asset, index) => (
+                    <Col span={12} key={index}>
+                      <Image src={`${S3_URL}/${asset?.s3Key}`} alt="" />
+                    </Col>
+                  ))}
+                </Row>
+              </Image.PreviewGroup>
+              <div className="h-[92px] flex flex-col items-end justify-end">
+                <Order />
+                <div className="py-1" />
+                <BookATestDriver />
+              </div>
             </div>
           </Affix>
         </Col>
--- components/products/ProductInfor.tsx
+++ components/products/ProductInfor.tsx
@@ -1,32 +1,49 @@
 import { useLangStore } from "@/stores/lang.store";
-import { Divider, Typography } from "antd";
+import { formatCurrency } from "@/utils/format-currency";
+import { formatDate } from "@/utils/format-date";
+import { Divider, Skeleton, Typography } from "antd";
 
-export default function ProductInfor() {
+interface Props {
+  dataInfor?: Record<string, any>;
+  isLoading?: boolean;
+}
+
+export default function ProductInfor({ dataInfor, isLoading }: Props) {
   const langStore = useLangStore((state: any) => state);
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-col space-y-3 w-full">
+        <Skeleton.Button active={true} className="!w-1/2" />
+        <Skeleton.Button active={true} className="!w-1/2" />
+      </div>
+    );
+  }
 
   return (
     <div>
       <div className="py-2">
         <Typography.Text className="font-thin !text-lg md:!text-2xl mr-2">
-          1953
+          {dataInfor?.manufactureYear}
         </Typography.Text>
         <Typography.Text className="font-semibold !text-lg md:!text-2xl mr-2">
-          Tojeiro MG
+          {dataInfor?.subTitle}
         </Typography.Text>
         <Typography.Text className="font-thin !text-lg md:!text-2xl mr-2">
-          Stirling Moss
+          {dataInfor?.shortTitle}
         </Typography.Text>
       </div>
 
       <div className="py-2">
         <Typography.Text className="font-semibold !text-lg md:!text-2xl">
-          $98,000
+          {formatCurrency(dataInfor?.price, localStorage.getItem("lang"))}
         </Typography.Text>
 
         <Divider type="vertical" className="!bg-black" />
 
         <Typography.Text className="font-thin !text-lg md:!text-2xl">
-          {langStore.lang.date}: {new Date().toDateString()}
+          {langStore.lang.date}:{" "}
+          {formatDate(dataInfor?.createAt, localStorage.getItem("lang"))}
         </Typography.Text>
       </div>
     </div>
